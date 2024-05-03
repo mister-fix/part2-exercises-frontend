87fba4c50ebfc83508d0ca49f2992ae9185a9c7b
Stephen <hellostephenwm@gmail.com>
2024-05-03 13:47:48 -0400

    Rendered persons and ability to add new persons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,52 @@
+import { useState } from "react";
+
 const App = () => {
-	return <div>App</div>;
+	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+	const [newName, setNewName] = useState("");
+
+	const handleNameChange = (event) => {
+		setNewName(event.target.value);
+	};
+
+	const addPerson = (event) => {
+		event.preventDefault();
+
+		const personObject = {
+			name: newName,
+		};
+
+		setPersons([...persons, personObject]);
+		/*
+		 * * The code above can also be done as such
+		 * * setPersons(persons.concat(personObject))
+		 */
+	};
+
+	return (
+		<div>
+			<h2>Phonebook</h2>
+
+			<form onSubmit={addPerson}>
+				<div>
+					name:{" "}
+					<input
+						value={newName}
+						onChange={handleNameChange}
+					/>
+				</div>
+
+				<div>
+					<button type="submit">add</button>
+				</div>
+			</form>
+
+			<h2>Numbers</h2>
+
+			{persons.map((person) => (
+				<p key={person.name}>{person.name}</p>
+			))}
+		</div>
+	);
 };
 
 export default App;
