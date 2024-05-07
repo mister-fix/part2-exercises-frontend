6a8ce8d91ee2f946df8e47ea98dad621f6021396
Stephen <hellostephenwm@gmail.com>
2024-05-07 17:23:28 -0400

    Added person removal method

--- src/services/persons.js
+++ src/services/persons.js
@@ -2,17 +2,23 @@ import axios from "axios";
 
 const baseUrl = "http://localhost:3001/persons";
 
-const getAll = () => {
+const getAllPersons = () => {
 	const request = axios.get(baseUrl);
 	return request.then((response) => response.data);
 };
 
-const create = (newObject) => {
+const createPerson = (newObject) => {
 	const request = axios.post(baseUrl, newObject);
 	return request.then((response) => response.data);
 };
 
+const removePerson = (id) => {
+	const request = axios.delete(`${baseUrl}/${id}`);
+	return request.then((response) => response.data);
+};
+
 export default {
-	getAll,
-	create,
+	getAllPersons,
+	createPerson,
+	removePerson,
 };
