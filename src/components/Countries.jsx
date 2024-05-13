efb578b34b9164bea4ebdcf820f8df33970aaf6f
Stephen <hellostephenwm@gmail.com>
2024-05-13 16:06:32 -0400

    Retrieving weather data for single country and passing it to country component

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,6 +1,25 @@
+import { useState, useEffect } from "react";
 import Country from "./Country";
+import weatherService from "../services/weather";
 
 const Countries = ({ value, countries, onSelectCountry }) => {
+	const [weatherData, setWeatherData] = useState();
+
+	useEffect(() => {
+		const retrieveWeatherData = (lat, lon) => {
+			weatherService
+				.getWeather(lat, lon)
+				.then((returnedWeather) => setWeatherData(returnedWeather));
+		};
+
+		if (countries && countries.length === 1) {
+			const [lat, lon] = countries[0].latlng;
+			retrieveWeatherData(lat, lon);
+		} else {
+			setWeatherData(null);
+		}
+	}, [countries]);
+
 	if (value && countries) {
 		if (countries.length === 1) {
 			return (
@@ -9,6 +28,7 @@ const Countries = ({ value, countries, onSelectCountry }) => {
 						<Country
 							key={country.name.common}
 							country={country}
+							weatherData={weatherData}
 						/>
 					))}
 				</div>
