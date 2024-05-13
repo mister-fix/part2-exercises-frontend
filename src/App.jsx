2726072509bc0689e2cb6c800a4f7f2fb47ef802
Stephen <hellostephenwm@gmail.com>
2024-05-13 14:41:47 -0400

    Added notification rendering

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import Notification from "./components/Notification";
 import personService from "./services/persons";
 
 const App = () => {
@@ -9,6 +10,10 @@ const App = () => {
 	const [filterBy, setFilterBy] = useState("");
 	const [newName, setNewName] = useState("");
 	const [newNumber, setNewNumber] = useState("");
+	const [notification, setNotification] = useState({
+		type: null,
+		message: null,
+	});
 
 	useEffect(() => {
 		personService.getAllPersons().then((initialPersons) => {
@@ -57,6 +62,16 @@ const App = () => {
 								person.id !== personToUpdate.id ? person : returnedPerson
 							)
 						);
+						setNewName("");
+						setNewNumber("");
+
+						setNotification({
+							type: "success",
+							message: `Updated ${personToUpdate.name}`,
+						});
+						setTimeout(() => {
+							setNotification({ type: null, message: null });
+						}, 3000);
 					});
 			}
 		} else {
@@ -64,23 +79,37 @@ const App = () => {
 				setPersons([...persons, returnedPerson]);
 				setNewName("");
 				setNewNumber("");
+
+				setNotification({
+					type: "success",
+					message: `Added ${returnedPerson.name}`,
+				});
+				setTimeout(() => {
+					setNotification({ type: null, message: null });
+				}, 3000);
 			});
 		}
 	};
 
-	const deletePerson = (id) => {
-		const person = persons.find((person) => person.id === id);
-		const accept = window.confirm(`Delete ${person.name}?`);
+	const deletePerson = (person) => {
+		const accept = window.confirm(`Remove ${person.name} from phonebook?`);
 
 		if (accept) {
 			personService
-				.removePerson(id)
+				.removePerson(person.id)
 				.then(() => {
-					setPersons(persons.filter((person) => person.id !== id));
+					setPersons(persons.filter((p) => p.id !== person.id));
+
+					setNotification({
+						type: "warning",
+						message: `${person.name} was removed`,
+					});
+					setTimeout(() => {
+						setNotification({ type: null, message: null });
+					}, 3000);
 				})
 				.catch((error) => {
-					alert(`${person.name} was already deleted`);
-					console.log(error.response);
+					console.log(error);
 				});
 		}
 	};
@@ -95,6 +124,8 @@ const App = () => {
 	return (
 		<div>
 			<h2>Phonebook</h2>
+
+			<Notification notification={notification} />
 
 			<Filter
 				filter={filterBy}
