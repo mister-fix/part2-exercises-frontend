c3ac973afa8b3701011696bf0e5ae576a03e669b
Stephen <hellostephenwm@gmail.com>
2024-05-13 15:29:44 -0400

    Rendering country details from search box results

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
+import { useState, useEffect } from "react";
+import SearchForm from "./components/SearchForm";
+import Countries from "./components/Countries";
+import countryService from "./services/countries";
+
 const App = () => {
-	return <div>App</div>;
+	const [value, setValue] = useState("");
+	const [countries, setCountries] = useState(null);
+
+	useEffect(() => {
+		if (value) {
+			countryService
+				.getAll()
+				.then((returnedCountries) => setCountries(returnedCountries));
+		}
+	});
+
+	const handleValue = (event) => {
+		setValue(event.target.value);
+	};
+
+	const showCountries = countries
+		? countries.filter((country) =>
+				country.name.common.toLowerCase().includes(value.toLowerCase())
+		  )
+		: null;
+
+	return (
+		<div>
+			<SearchForm
+				value={value}
+				onChangeHandler={handleValue}
+			/>
+
+			<Countries
+				value={value}
+				countries={showCountries}
+			/>
+		</div>
+	);
 };
 
 export default App;
